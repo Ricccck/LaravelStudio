3d9a45cc9fae98b5e69de4e05ea79b517a6aeeae
Riku Usui <rick.2000u@gmail.com>
2024-09-21 23:20:14 +0900

    :update: updated routes

--- LaravelStudioBE/routes/api.php
+++ LaravelStudioBE/routes/api.php
@@ -4,6 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
+Route::get('/test', function(){
+    return 'success';
+});
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
